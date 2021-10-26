7b911e2e973237413f12759537f13283d7e5f7f1
Jared <jaredbecker46@gmail.com>
2021-10-26 22:22:41 +0200

    Add logs table to application

---
+++ js/logs.js
@@ -0,0 +1,55 @@
+const logTable = document.getElementById('log_table');
+
+const getLogs = () => {
+    if (localStorage.getItem('logs') !== null) {
+        let logRows = [];
+        let logs = localStorage.getItem('logs');
+        logs = JSON.parse(logs);
+
+        logs.forEach(log => {
+            logRows.push(createLogRow(log))
+        });
+
+        logTable.innerHTML = logRows.join('');
+    }
+}
+
+const createLogRow = (log) => {
+    const time = formatLogTime(log.hour, log.min, log.sec);
+
+    return `
+        <tr>
+            <th scope="row">${log.id}</th>
+            <td>${log.title}</td>
+            <td>${time}</td>
+            <td>${log.date}</td>
+            <td>
+                <button type="button" class="btn btn-danger" onclick="deleteLog(${log.id})">Delete</button>
+            </td>
+        </tr>
+    `;
+}
+
+const formatLogTime = (hour, min, sec) => {
+    let time = '';
+
+    if (parseInt(hour) > 0) time += `${hour}hr `;
+    if (parseInt(min) > 0) time += `${min}min `;
+    if (parseInt(sec) > 0) time += `${sec}sec`;
+
+    return time;
+}
+
+const deleteLog = (id) => {
+    if (localStorage.getItem('logs') !== null) {
+        let logs = localStorage.getItem('logs');
+        logs = JSON.parse(logs);
+
+        logs = logs.filter(log => log.id !== id);
+
+        localStorage.setItem('logs', JSON.stringify(logs));
+        getLogs();
+    }
+}
+
+getLogs();
---
+++ js/stopwatch.js
@@ -0,0 +1,86 @@
+// Stopwatch variables
+let hour = 0;
+let min = 0;
+let sec = 0;
+let mil = 0;
+let stopwatch = document.querySelector('.stopwatch');
+
+const startStopwatch = () => {
+    stopwatchInterval = setInterval(() => {
+        if (mil < 975) {
+            mil += 25;
+        } else {
+            if (sec < 59) {
+                sec += 1;
+            } else {
+                sec = 0;
+                if (min < 59) {
+                    min += 1;
+                } else {
+                    min = 0;
+                    hour += 1;
+                }
+            }
+            mil = 0;
+        }
+
+        stopwatch.innerHTML = `
+            <span class="hours">${parseInt(hour) <= 9 ? '0' + hour : hour}</span> :
+            <span class="minutes">${parseInt(min) <= 9 ? '0' + min : min}</span> :
+            <span class="seconds">${parseInt(sec) <= 9 ? '0' + sec : sec}</span> :
+            <span class="milliseconds">${parseInt(mil) <= 9 ? '0' + hour : mil}</span>
+        `;
+
+    }, 25)
+}
+
+const pauseStopwatch = () => {
+    clearInterval(stopwatchInterval);
+}
+
+const completeLog = () => {
+    let logs = [];
+    let newLog = {
+        id: findNextId(),
+        title: document.getElementById('log_title').value,
+        hour: hour,
+        min: min,
+        sec: sec,
+        date: new Date().toLocaleDateString("en-US")
+    }
+
+    if (localStorage.getItem('logs') !== null) {
+        logs = JSON.parse(localStorage.getItem('logs'));
+    }
+
+    logs.push(newLog);
+    localStorage.setItem('logs', JSON.stringify(logs));
+
+    alert('Log saved successfully!');
+
+    hour = 0;
+    min = 0;
+    sec = 0;
+    mil = 0;
+
+    stopwatch.innerHTML = 'Let\'s Begin!';
+}
+
+const findNextId = () => {
+    if (localStorage.getItem('logs') !== null) {
+        let id = 0;
+        let logs = JSON.parse(localStorage.getItem('logs'));
+
+        logs.forEach(el => {
+            if (id > parseInt(el.id)) {
+                // do nothing
+            } else {
+                id = el.id
+            }
+        });
+
+        return id + 1
+    } else {
+        return 1;
+    }
+}
